7f78ccc49891f3066d73abb7623c8c24a5bb5f2e
russswest <joho141212@outlook.jp>
2018-03-10 11:10:12 +0900

    詳細と一覧作

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "/items" => "items#index"
+  get "/items/:id" => "items#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
